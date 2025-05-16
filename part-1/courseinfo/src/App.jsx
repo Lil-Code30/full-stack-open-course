7c6176cf9a6128552c7886110f114d7e13954210
Ismael Loko <lokoismael30@gmail.com>
2025-05-16 18:45:29 -0400

    1.3: Course Information step 3

--- part-1/courseinfo/src/App.jsx
+++ part-1/courseinfo/src/App.jsx
@@ -2,17 +2,24 @@
 function App() {
 
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using propos to pass data";
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
  
 
   // All components here
 
   const Header = (props) => {
+    console.log(props);
     return(
       <h1>{props.course}</h1>
     );
@@ -39,8 +46,8 @@ function App() {
   return (
     <div>
       <Header course={course}/>
-      <Content  part1= {part1} ex1={exercises1}  part2= {part2} ex2={exercises2} part3= {part3} ex3={exercises3}/>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content  part1= {part1.name} ex1={part1.exercises}  part2= {part2.name} ex2={part2.exercises} part3= {part3.name} ex3={part3.exercises}/>
+      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
     </div>
   )
 }

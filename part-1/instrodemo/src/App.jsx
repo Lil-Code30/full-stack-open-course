dec4447a5b00c3bc59ea46a94f212da95a90e93e
Ismael Loko <lokoismael30@gmail.com>
2025-05-25 18:12:25 -0400

    started part 1-c

--- part-1/instrodemo/src/App.jsx
+++ part-1/instrodemo/src/App.jsx
@@ -1,7 +1,14 @@
-const Hello = ({name, age}) => {
+const Hello = (props) => {
+
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+
   return (
     <div>
-      <p>Hello World {name}, you are {age} years old.</p>
+      <p>Hello World {props.name}, you are {props.age} years old.</p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }

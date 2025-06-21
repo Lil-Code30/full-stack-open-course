a2acdb291689518f1bec8b57dba9fa3b1329b998
Ismael Loko <lokoismael30@gmail.com>
2025-06-20 20:18:34 -0400

    created a separate Hello component

--- part-1/instrodemo/src/App.jsx
+++ part-1/instrodemo/src/App.jsx
@@ -2,22 +2,7 @@ import { useState } from "react";
 
 import DisplayCounter from "./components/DisplayCounter";
 import Button from "./components/Button";
-
-const Hello = (props) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear();
-    return yearNow - props.age;
-  };
-
-  return (
-    <div>
-      <p>
-        Hello World {props.name}, you are {props.age} years old.
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  );
-};
+import Hello from "./components/Hello";
 
 function App() {
   const [counter, setCounter] = useState(0);

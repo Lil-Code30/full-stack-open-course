023a714078db1588edc57ac2770ef830d1add7e9
Ismael Loko <lokoismael30@gmail.com>
2025-06-28 20:57:22 -0400

    2.4: Course information step 9

--- part-2/courseinfo/src/App.jsx
+++ part-2/courseinfo/src/App.jsx
@@ -1,43 +1,77 @@
 import Course from "./components/Course";
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  };
-  const total = course.parts.reduce((sum, part) => {
-    return sum + part.exercises;
-  }, 0);
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
+
+  // const total = courses.parts.reduce((sum, part) => {
+  //   return sum + part.exercises;
+  // }, 0);
 
   return (
     <div>
-      <h1>{course.name}</h1>
-      {course.parts.map((course) => (
-        <Course key={course.id} course={course} />
-      ))}
-      <h4>total of {total} exercises</h4>
+      <h1>Web Development Curriculum</h1>
+      {courses.map((course) => {
+        return (
+          <div key={course.id}>
+            <h2>{course.name}</h2>
+            {course.parts.map((part) => (
+              <p key={part.id}>
+                {part.name} {part.exercises}
+              </p>
+            ))}
+            <h4>
+              Total of{" "}
+              {course.parts.reduce((sum, part) => {
+                return sum + part.exercises;
+              }, 0)}{" "}
+              exercises
+            </h4>
+          </div>
+        );
+      })}
     </div>
   );
 };

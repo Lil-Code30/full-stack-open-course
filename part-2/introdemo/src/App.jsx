3c2625a816dd2fd6a49da6fe4fb49b1095f69525
Ismael Loko <lokoismael30@gmail.com>
2025-07-12 20:42:17 -0400

    Controlled component

--- part-2/introdemo/src/App.jsx
+++ part-2/introdemo/src/App.jsx
@@ -3,13 +3,23 @@ import Note from "./components/Note";
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
 
   const addNote = (e) => {
     e.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    };
 
-    console.log("Button clicked", e.target);
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
   };
-
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
   return (
     <div>
       <h1 className="text-2xl font-black">Notes</h1>
@@ -19,7 +29,12 @@ const App = (props) => {
         ))}
       </ul>
       <form onSubmit={addNote}>
-        <input />
+        <input
+          className="border my-2 rounded p-1"
+          onChange={handleNoteChange}
+          value={newNote}
+        />{" "}
+        <br />
         <button
           type="submit"
           className="hover:cursor-pointer bg-blue-600 border px-2 rounded text-white"

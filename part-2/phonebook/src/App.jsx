95308de38642532564420e78d3be57b2e880303b
Ismael Loko <lokoismael30@gmail.com>
2025-07-12 21:58:58 -0400

    2.8: The Phonebook Step 3

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newPerson, setNewPerson] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const newContact = {
       name: newPerson,
+      number: newNumber,
     };
 
     const inPhonebook = persons.some(
@@ -19,6 +23,7 @@ const App = () => {
     } else {
       setPersons(persons.concat(newContact));
       setNewPerson("");
+      setNewNumber("");
     }
   };
 
@@ -33,13 +38,26 @@ const App = () => {
             value={newPerson}
           />
         </div>
+        <br />
+        <div>
+          Number:{" "}
+          <input
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
+        </div>
+        <br />
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((item, i) => {
-        return <p key={i}>{item.name}</p>;
+        return (
+          <p key={i}>
+            {item.name} : {item.number}
+          </p>
+        );
       })}
     </div>
   );

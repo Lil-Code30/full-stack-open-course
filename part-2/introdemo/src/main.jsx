d62a34ad6306dde04aa8d2b98b1a4ba8880f4a09
Ismael Loko <lokoismael30@gmail.com>
2025-07-12 23:22:43 -0400

    npm

--- part-2/introdemo/src/main.jsx
+++ part-2/introdemo/src/main.jsx
@@ -1,7 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import axios from "axios";
 
 import App from "./App.jsx";
+
+const promise = axios.get("http://localhost:3001/notes");
+console.log(promise);
+
+const promise2 = axios.get("http://localhost:3001/foobar");
+console.log(promise2);
 
 const notes = [
   {
